833629b2dcac90ddea6b9ac1c7c1491236b032ca
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2025-03-25 22:41:35 +0300

    Feature: Import Expense

--- src/main/java/site/easy/to/build/crm/controller/ImportController.java
+++ src/main/java/site/easy/to/build/crm/controller/ImportController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import site.easy.to.build.crm.service.importt.BudgetImportService;
 import site.easy.to.build.crm.service.importt.CustomerImportService;
 import site.easy.to.build.crm.service.importt.CustomerImportService.ImportResult;
+import site.easy.to.build.crm.service.importt.ExpenseImportService;
 import site.easy.to.build.crm.service.importt.ImportService;
 
 @Controller
@@ -21,13 +22,15 @@ public class ImportController {
     private final ImportService importService;
     CustomerImportService customerImportService;
     BudgetImportService budgetImportService;
+    ExpenseImportService expenseImportService;
 
     @Autowired
     public ImportController(ImportService importService, CustomerImportService customerImportService,
-                            BudgetImportService budgetImportService) {
+                            BudgetImportService budgetImportService, ExpenseImportService expenseImportService) {
         this.importService = importService;
         this.customerImportService= customerImportService;
         this.budgetImportService= budgetImportService;
+        this.expenseImportService= expenseImportService;
     }
     
     @GetMapping("/")
@@ -65,6 +68,22 @@ public class ImportController {
     public String importbudgets(@RequestParam("budgetCsv") MultipartFile budgetCsv, Model model) {
         try {
             site.easy.to.build.crm.entity.ImportResult result = budgetImportService.importBudgets(budgetCsv);
+            model.addAttribute("importResult", result);
+            if (result.isSuccess()) {
+                model.addAttribute("successMessage", result.getMessage());
+            } else {
+                model.addAttribute("errorMessage", result.getMessage());
+            }
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Une erreur inattendue s'est produite : " + e.getMessage());
+        }
+        return "import/import"; 
+    }
+
+    @PostMapping("/expense")
+    public String importExpenses(@RequestParam("expenseCsv") MultipartFile expenseCsv, Model model) {
+        try {
+            site.easy.to.build.crm.entity.ImportResult result = expenseImportService.importExpenses(expenseCsv);
             model.addAttribute("importResult", result);
             if (result.isSuccess()) {
                 model.addAttribute("successMessage", result.getMessage());

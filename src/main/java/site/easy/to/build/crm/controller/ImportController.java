1a62a8b2731ba5968d5e00c20c12233cc3f92a12
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2025-03-25 13:32:09 +0300

    Feature: Import Customer

--- src/main/java/site/easy/to/build/crm/controller/ImportController.java
+++ src/main/java/site/easy/to/build/crm/controller/ImportController.java
@@ -2,12 +2,15 @@ package site.easy.to.build.crm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import site.easy.to.build.crm.service.importt.CustomerImportService;
+import site.easy.to.build.crm.service.importt.CustomerImportService.ImportResult;
 import site.easy.to.build.crm.service.importt.ImportService;
 
 @Controller
@@ -15,25 +18,43 @@ import site.easy.to.build.crm.service.importt.ImportService;
 public class ImportController {
 
     private final ImportService importService;
+    CustomerImportService customerImportService;
 
     @Autowired
-    public ImportController(ImportService importService) {
+    public ImportController(ImportService importService, CustomerImportService customerImportService) {
         this.importService = importService;
+        this.customerImportService= customerImportService;
     }
     
-    @GetMapping
+    @GetMapping("/")
     public String index(){
         return "import/import";
     }
 
-    @PostMapping("/employee")
-    public String importEmployees(@RequestParam("filePath") MultipartFile filePath) {
+    // @PostMapping("/employee")
+    // public String importEmployees(@RequestParam("filePath") MultipartFile filePath) {
+    //     try {
+    //         importService.importEmployees(filePath);
+    //     } catch (Exception e){
+    //         return "error/500";
+    //     }
+    //     return "redirect:/";
+    // }
+
+    @PostMapping("/customer")
+    public String importCustomers(@RequestParam("filePath") MultipartFile filePath, Model model) {
         try {
-            importService.importEmployees(filePath);
-        } catch (Exception e){
-            return "error/500";
+            ImportResult result = customerImportService.importCustomers(filePath);
+            model.addAttribute("importResult", result);
+            if (result.isSuccess()) {
+                model.addAttribute("successMessage", result.getMessage());
+            } else {
+                model.addAttribute("errorMessage", result.getMessage());
+            }
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Une erreur inattendue s'est produite : " + e.getMessage());
         }
-        return "redirect:/";
+        return "import/import"; 
     }
 
 }
--- src/main/java/site/easy/to/build/crm/service/importt/ImportService.java
+++ src/main/java/site/easy/to/build/crm/service/importt/ImportService.java
@@ -15,6 +15,7 @@ import java.io.InputStreamReader;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class ImportService {
@@ -25,170 +26,19 @@ public class ImportService {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    @Transactional
-    public void importEmployees(MultipartFile file) throws IOException, CsvValidationException {
-        try {
-            System.out.println("Mamomboka");
-            createTempTable();
-            List<EmployeeCsvDto> employees = parseCsv(file);
-            insertIntoTempTable(employees);
-            validateData();
-            insertIntoEmployeeTable();
-            System.out.println("Vita import");
-        } finally {
-            cleanUpTempTable();
-        }
-    }
-
-    private void createTempTable() {
-        System.out.println("Creation table temporaire");
-        jdbcTemplate.execute("CREATE TEMPORARY TABLE employeeTemp ("
-                + "username VARCHAR(45) NOT NULL,"
-                + "first_name VARCHAR(45) NOT NULL,"
-                + "last_name VARCHAR(45) NOT NULL,"
-                + "email VARCHAR(45) NOT NULL,"
-                + "password VARCHAR(80) NOT NULL,"
-                + "provider VARCHAR(45))");
-        System.out.println("Table temporaire creer");
-    }
-
-    private List<EmployeeCsvDto> parseCsv(MultipartFile file) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            CsvToBean<EmployeeCsvDto> csvToBean = new CsvToBeanBuilder<EmployeeCsvDto>(reader)
-                    .withType(EmployeeCsvDto.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-            return csvToBean.parse();
-        }
-    }
-
-    private void insertIntoTempTable(List<EmployeeCsvDto> employees) {
-        System.out.println("Insertion dans table temp");
-        String sql = "INSERT INTO employeeTemp (username, first_name, last_name, email, password, provider) VALUES (?, ?, ?, ?, ?, ?)";
+    public static String generateRandomString() {
+        Random random = new Random();
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        int longueur = random.nextInt(16); // Génère une longueur entre 0 et 15
         
-        try{
-            jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
-                @Override
-                public void setValues(PreparedStatement ps, int i) throws SQLException {
-                    EmployeeCsvDto employee = employees.get(i);
-                    System.out.println(employee.getUsername()+""+employee.getFirstName()+""+employee.getLastName()+""+employee.getEmail()+""+employee.getPassword()+""+employee.getProvider());
-                    
-                    ps.setString(1, employee.getUsername());
-                    ps.setString(2, employee.getFirstName());
-                    ps.setString(3, employee.getLastName());
-                    ps.setString(4, employee.getEmail());
-                    ps.setString(5, employee.getPassword());
-                    ps.setString(6, employee.getProvider());
-                }
-    
-                @Override
-                public int getBatchSize() {
-                    return employees.size();
-                }
-            });
+        StringBuilder chaine = new StringBuilder();
+        for (int i = 0; i < longueur; i++) {
+            int index = random.nextInt(caracteres.length());
+            chaine.append(caracteres.charAt(index));
         }
-        catch(Exception e){ 
-            System.out.println("Erreur insertion donnees: "+ e.getMessage());
-        }
-        System.out.println("Insertion dans table temp terminee");
-
-    }
-
-    private void validateData() {
-        System.out.println("Validation data");
-        validateRequiredFields();
-        validateUniqueEmails();
-        validateEmailUniquenessInDatabase();
-        System.out.println("Validation terminee");
-    }
-
-    private void validateRequiredFields() {
-        String sql = "SELECT COUNT(*) FROM employeeTemp "
-                + "WHERE username = '' OR first_name = '' OR last_name = '' OR email = '' OR password = ''";
         
-        int invalidCount = jdbcTemplate.queryForObject(sql, Integer.class);
-        if (invalidCount > 0) {
-            System.out.println("Certains champs obligatoires sont vides");
-            throw new RuntimeException("Certains champs obligatoires sont vides");
-        }
+        return chaine.toString();
     }
 
-    private void validateUniqueEmails() {
-        String sql = "SELECT COUNT(*) FROM (SELECT email FROM employeeTemp GROUP BY email HAVING COUNT(*) > 1) duplicates";
-        
-        int duplicateCount = jdbcTemplate.queryForObject(sql, Integer.class);
-        if (duplicateCount > 0) {
-            throw new RuntimeException("Emails en double dans le fichier CSV");
-        }
-    }
-
-    private void validateEmailUniquenessInDatabase() {
-        String sql = "SELECT COUNT(e.id) FROM employee e "
-                + "INNER JOIN employeeTemp et ON e.email = et.email";
-        
-        int existingCount = jdbcTemplate.queryForObject(sql, Integer.class);
-        if (existingCount > 0) {
-            throw new RuntimeException("Certains emails existent déjà dans la base");
-        }
-    }
-
-    private void insertIntoEmployeeTable() {
-        String sql = "INSERT INTO employee (username, first_name, last_name, email, password, provider) "
-                + "SELECT username, first_name, last_name, email, password, provider FROM employeeTemp";
-        
-        jdbcTemplate.execute(sql);
-    }
-
-    private void cleanUpTempTable() {
-        jdbcTemplate.execute("DROP TEMPORARY TABLE IF EXISTS employeeTemp");
-    }
-
-    // Classe DTO pour la lecture CSV
-    public static class EmployeeCsvDto {
-        private String username;
-        private String firstName;
-        private String lastName;
-        private String email;
-        private String password;
-        private String provider;
-
-        public String getUsername() {
-            return username;
-        }
-        public void setUsername(String username) {
-            this.username = username;
-        }
-        public String getFirstName() {
-            return firstName;
-        }
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-        public String getLastName() {
-            return lastName;
-        }
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-        public String getEmail() {
-            return email;
-        }
-        public void setEmail(String email) {
-            this.email = email;
-        }
-        public String getPassword() {
-            return password;
-        }
-        public void setPassword(String password) {
-            this.password = password;
-        }
-        public String getProvider() {
-            return provider;
-        }
-        public void setProvider(String provider) {
-            this.provider = provider;
-        }
-
-        // Getters et Setters
-    }
+  
 }
